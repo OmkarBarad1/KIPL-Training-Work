eb281053a3cac9d3d73a13495623bec45e77c22e
OmkarBarad1 <baradomkar871@gmail.com>
2025-12-11 16:46:08 +0530

    Initial commit

---
+++ C#_Fundamental.cs
@@ -0,0 +1,133 @@
+// // Online C# Editor for free
+// // Write, Edit and Run your C# code using C# Online Compiler
+
+ using System;
+
+ public class HelloWorld
+ {
+     public static void Main(string[] args)
+    {
+//         // Console.WriteLine ("please enter your name");
+//         // string MyName= Console.ReadLine();
+//         // Console.WriteLine("Hello " + MyName);
+        
+        
+//         // Console.WriteLine("Min value for int " + int.MinValue + "\nMax value for int " + int.MaxValue);
+//         // Console.WriteLine("Min value for int " + double.MinValue + "\nMax value for int " + double.MaxValue);
+//         // Console.WriteLine("Min value for int " + long.MinValue + "\nMax value for int " + long.MaxValue);
+        
+//         //escape sequence
+        
+//         // Console.WriteLine("My Name Is \"Omkar\"");
+//         // Console.WriteLine("\"Honda\" And \"BMW\" are Car Companies" );
+//         // //If-Else
+//         // Console.WriteLine("Enter Your Age");
+        
+//         // int age=Convert.ToInt32(Console.ReadLine());
+
+//     //         1. Null Handling
+//     // 	int.Parse(null)
+
+//     // 	❌ Throws an exception (ArgumentNullException)
+
+//     // 	Convert.ToInt32(null)
+
+//     // 	✅ Returns 0
+//     // 	(No exception)
+	
+
+// 	//     Type Flexibility
+// // int.Parse()
+// // ✔ Only works with string.
+// // Convert.ToInt32()
+// // ✔ Works with many types, like:
+// // string
+// // bool
+// // double
+// // object
+// // decimal
+
+
+//         // //If-Else
+//         // Console.WriteLine("Enter Your Age");
+        
+//         // int age=Convert.ToInt32(Console.ReadLine());
+//         //  if(age<18 && age>0){
+//         //      Console.WriteLine("Your Not Eligiable");
+//         //  }
+//         //  else if(age>18 && age<100){
+//         //      Console.WriteLine("Your Eligiable");
+//         //  }
+//         //  else{
+//         //      Console.WriteLine("Enter the valid Age");
+//         //  }
+        
+//         //Switch case
+//         int TotalCost=0;
+//         Start:
+//         Console.WriteLine("Please select the coffee size: 1.Small, 2.Medium, 3.Large");
+//         int choice=Convert.ToInt32(Console.ReadLine());
+//         switch(choice){
+//             case 1:
+//                 Console.WriteLine("your Coffee Cost is 25 ");
+//                 TotalCost+=25;
+//                 break;
+//             case 2:
+//                 Console.WriteLine("your Coffee Cost is 50 ");
+//                 TotalCost+=50;
+//                 break;
+//             case 3:
+//                 Console.WriteLine("your Coffee Cost is 75 ");
+//                 TotalCost+=75;
+//                 break;
+//             default:
+//                 Console.WriteLine("Please Enter the valid Number");
+//                 goto Start;
+//         }
+//         Decide:
+//         Console.WriteLine("Do You Want To Buy Anothe Coffee \"Yes\" \"No\"");
+//         string UserDecision=Console.ReadLine();
+//         switch(UserDecision.ToUpper()){
+//             case "YES":
+//                 goto Start;
+//              //No need of break because program pointer will go to the start;
+//              case "NO":
+//                 Console.WriteLine("Thank you for the shopping");
+//                 Console.WriteLine($"Your Total Cost for coffee is {TotalCost} Rs.");
+//                 break;
+//             default:
+//             goto Decide;
+                
+//         }
+    //  Console.WriteLine("Enter the Number in which You want to Print the even Numbers");
+    //      int Target=Convert.ToInt32(Console.ReadLine());
+    //     int Start=0;
+    //     while(Start<=Target){
+    //         Console.Write(Start + " ");
+    //         Start+=2;
+    //     }
+    // Console.WriteLine("Enter the Number in which You want to Print the even Numbers");
+    //      int Target=Convert.ToInt32(Console.ReadLine());
+    //     int Start=0;
+    //     do{
+    //         Console.Write(Start + " ");
+    //         Start+=2;
+    //     }while(Start<=Target);
+    
+    
+        // int[] Nums=new int[3];
+        // Nums[0]=1001;
+        // Nums[1]=1002;
+        // Nums[2]=1003;
+        // for(int i=0;i<Nums.Length;i++){
+        //     Console.WriteLine(Nums[i]);
+        // }
+        
+        
+        // foreach(int Num in Nums){
+        //     Console.WriteLine(Num);
+        // }
+      }
+    
+     
+ }
